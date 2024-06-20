21a72330a1d2f52022b11fb05c5abaf72f1970c6
itsmikesharescode <mikejohneviota@gmail.com>
2024-06-21 01:23:57 +0800

    add login server actions

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,4 +1,4 @@
-import { superValidate } from 'sveltekit-superforms';
+import { message, superValidate } from 'sveltekit-superforms';
 import type { Actions, PageServerLoad } from './$types';
 import { zod } from 'sveltekit-superforms/adapters';
 import { forgotPwdSchema, loginSchema, registerSchema } from '$lib/schema';
@@ -14,11 +14,25 @@ export const load: PageServerLoad = async () => {
 
 export const actions: Actions = {
 	login: async (event) => {
+		const {
+			locals: { supabase }
+		} = event;
+
 		const form = await superValidate(event, zod(loginSchema));
 
 		if (!form.valid) return fail(401, { form });
 
-		return { form };
+		const {
+			data: { user },
+			error
+		} = await supabase.auth.signInWithPassword({
+			email: form.data.email,
+			password: form.data.password
+		});
+
+		if (error) return message(form, { status: 401, msg: error.message });
+		else if (user) return message(form, { status: 200, msg: 'Welcome back.' });
+		else return message(form, { status: 401, msg: 'Server error.' });
 	},
 
 	register: async (event) => {
