778aa6a65a8051aaf9d2a360b13eeb33be16da59
itsmikesharescode <mikejohneviota@gmail.com>
2024-06-20 17:57:37 +0800

    add login UI

--- src/lib/components/struct/static/login.svelte
+++ src/lib/components/struct/static/login.svelte
@@ -1,4 +1,54 @@
 <script lang="ts">
+	import { loginSchema, type LoginSchema } from '$lib/schema';
+	import { superForm, type Infer, type SuperValidated } from 'sveltekit-superforms';
+	import { zodClient } from 'sveltekit-superforms/adapters';
+	import * as Form from '$lib/components/ui/form';
+	import { Input } from '$lib/components/ui/input';
+	import Separator from '$lib/components/ui/separator/separator.svelte';
+	import Button from '$lib/components/ui/button/button.svelte';
+
+	interface PropType {
+		loginForm: SuperValidated<Infer<LoginSchema>>;
+	}
+
+	const { loginForm }: PropType = $props();
+
+	const form = superForm(loginForm, { validators: zodClient(loginSchema) });
+
+	const { form: formData, enhance, submitting, message } = form;
 </script>
 
-<div class=""></div>
+<div class="mx-auto grid w-[300px] gap-[20px] sm:w-[350px]">
+	<div class="">
+		<h3 class="scroll-m-20 text-center text-2xl font-semibold tracking-tight">Log in</h3>
+		<p class="text-center text-sm leading-7 text-muted-foreground">
+			Enter your email and password to log in
+		</p>
+	</div>
+
+	<form method="POST" use:enhance class="grid gap-[10px]">
+		<Form.Field {form} name="email">
+			<Form.Control let:attrs>
+				<Form.Label>Email</Form.Label>
+				<Input type="email" {...attrs} bind:value={$formData.email} />
+			</Form.Control>
+			<Form.Description>Enter your email.</Form.Description>
+			<Form.FieldErrors />
+		</Form.Field>
+
+		<Form.Field {form} name="password">
+			<Form.Control let:attrs>
+				<Form.Label>Password</Form.Label>
+				<Input type="password" {...attrs} bind:value={$formData.password} />
+			</Form.Control>
+			<Form.Description>Enter your password.</Form.Description>
+			<Form.FieldErrors />
+		</Form.Field>
+		<Form.Button>Log In</Form.Button>
+	</form>
+
+	<Separator />
+
+	<Button variant="secondary">Forgot Password</Button>
+	<Button variant="secondary">Create Account</Button>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,14 +3,16 @@
 	import ForgotPassword from '$lib/components/struct/static/forgot-password.svelte';
 	import Login from '$lib/components/struct/static/login.svelte';
 	import Register from '$lib/components/struct/static/register.svelte';
+
+	const { data } = $props();
 </script>
 
-<div class="">
+<div class="flex min-h-screen flex-col justify-center p-[1rem]">
 	{#if $page.url.searchParams.get('q') === 'register'}
 		<Register />
 	{:else if $page.url.searchParams.get('q') === 'forgot-password'}
 		<ForgotPassword />
 	{:else}
-		<Login />
+		<Login loginForm={data.loginForm} />
 	{/if}
 </div>
