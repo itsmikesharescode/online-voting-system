a0f01eda9c6f213ed0cbd22e7de289ba9f9943fd
itsmikesharescode <mikejohneviota@gmail.com>
2024-06-20 17:44:11 +0800

    add registerform

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,12 +1,13 @@
 import { superValidate } from 'sveltekit-superforms';
 import type { Actions, PageServerLoad } from './$types';
 import { zod } from 'sveltekit-superforms/adapters';
-import { loginSchema } from '$lib/schema';
+import { loginSchema, registerSchema } from '$lib/schema';
 import { fail } from '@sveltejs/kit';
 
 export const load: PageServerLoad = async () => {
 	return {
-		loginForm: await superValidate(zod(loginSchema), { id: crypto.randomUUID() })
+		loginForm: await superValidate(zod(loginSchema), { id: crypto.randomUUID() }),
+		registerForm: await superValidate(zod(registerSchema), { id: crypto.randomUUID() })
 	};
 };
 
