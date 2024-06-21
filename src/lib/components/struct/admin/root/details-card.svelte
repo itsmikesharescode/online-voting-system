20d532f4ec35c5880511c4af4149c793bae2baae
itsmikesharescode <mikejohneviota@gmail.com>
2024-06-21 23:33:33 +0800

    add cards

---
+++ src/lib/components/struct/admin/root/details-card.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	import * as Card from '$lib/components/ui/card';
+	import type { Snippet } from 'svelte';
+
+	interface PropType {
+		title: string;
+		icon: Snippet;
+		value: number;
+	}
+
+	const { title, icon, value }: PropType = $props();
+</script>
+
+<Card.Root>
+	<Card.Header>
+		<Card.Title class="flex items-center justify-between">
+			{title}
+			{@render icon()}
+		</Card.Title>
+	</Card.Header>
+	<Card.Content class="grid gap-[20px]">
+		<h1 class="scroll-m-20 text-center text-4xl font-extrabold tracking-tight lg:text-5xl">30</h1>
+
+		<Card.Description>+10 Today</Card.Description>
+	</Card.Content>
+</Card.Root>
--- src/routes/admin/+page.svelte
+++ src/routes/admin/+page.svelte
@@ -1,7 +1,24 @@
 <script lang="ts">
 	import { routeState } from '$lib/runes.svelte';
+	import DetailsCard from '$lib/components/struct/admin/root/details-card.svelte';
+	import { UserCheck, NotebookText } from 'lucide-svelte';
 
 	routeState.setActiveRoute('/admin');
 </script>
 
-<div class="min-h-screen md:border-l-[1px]"></div>
+{#snippet userCheck()}
+	<UserCheck />
+{/snippet}
+
+{#snippet noteBookText()}
+	<NotebookText />
+{/snippet}
+
+<div class="min-h-screen p-[1rem] md:border-l-[1px]">
+	<div class="grid gap-[10px] md:grid-cols-2 lg:grid-cols-4">
+		<DetailsCard title="Total Voter" icon={userCheck} value={30} />
+		<DetailsCard title="Total Voted" icon={userCheck} value={30} />
+		<DetailsCard title="Total Positions" icon={noteBookText} value={30} />
+		<DetailsCard title="Total Candidates" icon={noteBookText} value={30} />
+	</div>
+</div>
