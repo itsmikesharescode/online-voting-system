6b765c957ad945bc8380354d8987ca0d6d2511b8
itsmikesharescode <mikejohneviota@gmail.com>
2024-06-23 07:37:07 +0800

    add voters UI

--- src/routes/admin/voters/+page.svelte
+++ src/routes/admin/voters/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import CreateVoter from '$lib/components/struct/admin/voters/create-voter.svelte';
+	import RenderVoters from '$lib/components/struct/admin/voters/render-voters.svelte';
 	import { routeState } from '$lib/runes.svelte.js';
 	import { onMount } from 'svelte';
 
@@ -8,4 +9,8 @@
 
 <div class="min-h-screen border-slate-700 p-[1rem] md:border-l-[1px]">
 	<CreateVoter createVoterForm={data.createVoterForm} user={data.user} />
+
+	<div class="mt-[20px]">
+		<RenderVoters />
+	</div>
 </div>
