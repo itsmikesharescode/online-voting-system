21f6e23f167ef3b5e0fc9a47faa6cdbcbc5df95c
itsmikesharescode <mikejohneviota@gmail.com>
2024-06-22 22:58:22 +0800

    add logout at liveresult

--- src/lib/components/struct/admin/live_result/result-card.svelte
+++ src/lib/components/struct/admin/live_result/result-card.svelte
@@ -3,7 +3,7 @@
 	import BarChart from './bar-chart.svelte';
 </script>
 
-<div class="grid gap-[10px] rounded-lg border-[1px] border-secondary p-[1rem]">
+<div class="grid gap-[10px] rounded-lg border-[1px] border-slate-700 p-[1rem]">
 	<h4 class="scroll-m-20 text-xl font-semibold tracking-tight">Senators</h4>
 
 	<div class="h-[20dvh]">
@@ -13,11 +13,11 @@
 	<div class="grid gap-[10px]">
 		<div class="grid grid-cols-[70%,30%]">
 			<span>
-				<p class="font-semibold text-muted-foreground">Name</p>
+				<p class="font-semibold">Name</p>
 			</span>
 
 			<span>
-				<p class="mx-auto max-w-fit text-center font-semibold text-muted-foreground">Count</p>
+				<p class="mx-auto max-w-fit text-center font-semibold">Count</p>
 			</span>
 		</div>
 
@@ -29,11 +29,11 @@
 							<Avatar.Image src="https://github.com/shadcn.png" alt="@shadcn" />
 							<Avatar.Fallback>CN</Avatar.Fallback>
 						</Avatar.Root>
-						<p class="font-semibold text-muted-foreground">Eviota, Mike John B</p>
+						<p class="font-semibold">Eviota, Mike John B</p>
 					</span>
 
 					<span>
-						<p class="mx-auto max-w-fit text-center font-semibold text-muted-foreground">30</p>
+						<p class="mx-auto max-w-fit text-center font-semibold">30</p>
 					</span>
 				</div>
 			{/each}
--- src/routes/admin/live-result/+page.svelte
+++ src/routes/admin/live-result/+page.svelte
@@ -3,7 +3,7 @@
 	import ResultCard from '$lib/components/struct/admin/live_result/result-card.svelte';
 </script>
 
-<div class="min-h-screen p-[1rem] md:border-l-[1px]">
+<div class="min-h-screen border-slate-700 p-[1rem] md:border-l-[1px]">
 	<div class="grid gap-[5px] lg:grid-cols-3">
 		{#each Array(5) as _}
 			<ResultCard />
