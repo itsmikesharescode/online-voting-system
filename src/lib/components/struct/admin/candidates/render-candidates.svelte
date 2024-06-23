c22d6ea3661a638fe9a58ca8d1f3f42d236c7e89
itsmikesharescode <mikejohneviota@gmail.com>
2024-06-23 11:07:15 +0800

    update candidate UI

--- src/lib/components/struct/admin/candidates/render-candidates.svelte
+++ src/lib/components/struct/admin/candidates/render-candidates.svelte
@@ -9,16 +9,15 @@
 
 <div class="grid gap-[10px]">
 	<div
-		class="top-0 z-10 grid grid-cols-[90%,8%] gap-[2%] border-b-[1px] border-slate-700 p-[10px] backdrop-blur-lg md:grid-cols-[42.5%,42.5%,5%] lg:sticky lg:grid-cols-[37.5%,37.5%,15%,10%]"
+		class="top-0 z-10 grid grid-cols-[90%,8%] gap-[2%] border-b-[1px] border-slate-700 p-[10px] backdrop-blur-lg md:grid-cols-[61%,21%,5%] lg:sticky lg:grid-cols-[75%,15%,8%]"
 	>
-		<p class="">Voter Name</p>
-		<p class="hidden md:block">Email</p>
-		<p class="hidden lg:block">Created At</p>
+		<p class="">Candidate Name</p>
+		<p class="hidden md:block">Created At</p>
 	</div>
 
 	{#each Array(30) as _}
 		<div
-			class=" grid grid-cols-[90%,8%] items-center gap-[2%] p-[10px] md:grid-cols-[41%,41%,5%] lg:grid-cols-[37.5%,37.5%,15%,8%]"
+			class=" grid grid-cols-[90%,8%] items-center gap-[2%] p-[10px] md:grid-cols-[61%,21%,5%] lg:grid-cols-[75%,15%,8%]"
 		>
 			<div class="flex items-center gap-[5px]">
 				<Avatar.Root>
@@ -27,8 +26,7 @@
 				</Avatar.Root>
 				<p class="">Mike John Eviota</p>
 			</div>
-			<p class="hidden md:block">mike@gmail.com</p>
-			<p class="hidden lg:block">Nov 1 2024</p>
+			<p class="hidden md:block">Nov 1 2024</p>
 
 			<div class="w-fit">
 				<DropdownMenu.Root>
