bdbbde686b76e1ac2ab02ac6f7c682e7d0524dbc
itsmikesharescode <mikejohneviota@gmail.com>
2024-06-26 11:06:39 +0800

    add footer

--- src/lib/components/general/footer.svelte
+++ src/lib/components/general/footer.svelte
@@ -5,6 +5,15 @@
 <div class="border-t-[1px] bg-stone-950 p-[4rem]">
 	<div class=" space-y-2 text-center text-sm text-muted-foreground">
 		<DarkMode />
-		<p>Made with ❤️ by Mike John <b>Eviota</b></p>
+		<p>
+			Made with ❤️ by
+			<a
+				href="https://mike-shares-code.vercel.app/"
+				target="_blank"
+				class="border-b-[1px] border-slate-700"
+			>
+				Mike John <b>Eviota</b>
+			</a>
+		</p>
 	</div>
 </div>
